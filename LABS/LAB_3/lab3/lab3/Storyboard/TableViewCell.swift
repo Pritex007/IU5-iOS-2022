b0d421bd67d356edb7e59993ef574172113f8c91
Pritex007 <pritex009@gmail.com>
2022-03-21 22:43:39 +0300

    lab4

--- LABS/LAB_3/lab3/lab3/Storyboard/TableViewCell.swift
+++ LABS/LAB_3/lab3/lab3/Storyboard/TableViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-
+    
     @IBOutlet private weak var LogoView: UIImageView!
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var subtitleLabel: UILabel!
--- LABS/LAB_3/lab3/lab3/Storyboard/TableViewController.swift
+++ LABS/LAB_3/lab3/lab3/Storyboard/TableViewController.swift
@@ -11,6 +11,8 @@ class TableViewController: UIViewController {
 
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var footer: UILabel!
     
     private let CellProp = [
                             (title: "Profile", subtitle: "Account", image: "person"),
@@ -27,8 +29,12 @@ class TableViewController: UIViewController {
     
     
     private func setupTable() {
+        header.text = "Header"
+        footer.text = "Footer"
+        
         tableView.delegate = self
         tableView.dataSource = self
+        
         tableView.register(TableViewCell.self, forCellReuseIdentifier: "test")
     }
 }
@@ -38,25 +44,24 @@ extension TableViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 4
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "testCell1") as? TableViewCell
         cell?.configure(title: CellProp[indexPath.row].title, subtitle: CellProp[indexPath.row].subtitle, image: UIImage(systemName: CellProp[indexPath.row].image)!)
-        
         return cell!
     }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard indexPath.row != 0 else {
-        let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "FuncView")
-        present(viewController, animated: true)
-        return
+            let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "FuncView")
+            present(viewController, animated: true)
+            return
         }
     }
-    
 }
 extension TableViewController: UITableViewDelegate {
 }
-    
-
