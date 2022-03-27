d9bddc2c692749faac1c6c6b175ef816bc0222e3
Pritex007 <pritex009@gmail.com>
2022-03-27 18:56:14 +0300

    Revert "Merge pull request #2 from Pritex007/lab/number2"
    This reverts commit c51ca69243658eab404a3cfa3e456179305bc38a, reversing
    changes made to 81fcc9381b46160398363cc3d59820c1d1ccb760.

--- LABS/LAB_2/Shipment.swift
+++ LABS/LAB_2/Shipment.swift
@@ -22,34 +22,22 @@ import Foundation
 final class Shipment {
     static func fruitShipment(pearsCount: Int, applesCount: Int, completion: @escaping (Int) -> ()) {
         var resultFruitCount = 0
-        let group = DispatchGroup()
-        let lock = NSLock()
-        
-        group.enter()
+
         let pearsThread = Thread {
             for _ in 1...pearsCount {
-                lock.lock()
                 resultFruitCount += 1
-                lock.unlock()
             }
-            group.leave()
         }
-        
-        group.enter()
+
         let applesThread = Thread {
             for _ in 1...applesCount {
-                lock.lock()
                 resultFruitCount += 1
-                lock.unlock()
             }
-            group.leave()
         }
 
         pearsThread.start()
         applesThread.start()
-        
-        group.notify(queue: .main) {
-           completion(resultFruitCount)
-        }
+
+        completion(resultFruitCount)
     }
 }
